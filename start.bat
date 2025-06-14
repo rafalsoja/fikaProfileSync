4da74b708bff3d42661ed6a97cf99a0d285e4d65
Cichaj <em.rvso@gmail.com>
2025-06-14 19:03:55 +0200

    restrukturyzacja pierdolnika

---
+++ start.bat
@@ -0,0 +1,76 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:: Change the game directory if needed
+set "GAME_DIR=C:\GAMES\Escape From Tarkov"
+
+set "URL1=http://172.22.89.147:6969"
+set "URL2=http://172.22.59.102:6969"
+
+set "RESPONDING=0"
+
+curl --max-time 1 --silent --output nul "%URL1%/singleplayer/settings/version"
+if !errorlevel! == 0 (
+    echo [INFO] Server %URL1% is responding.
+    set "RESPONDING=1"
+    set "SELECTED_URL=%URL1%"
+)
+
+if "!RESPONDING!" == "0" (
+    curl --max-time 1 --silent --output nul "%URL2%/singleplayer/settings/version"
+    if !errorlevel! == 0 (
+        echo [INFO] Server %URL2% is responding.
+        set "RESPONDING=1"
+        set "SELECTED_URL=%URL2%"
+    )
+)
+
+if "!RESPONDING!" == "0" (
+    echo [INFO] None of the servers are responding. Launching local server...
+
+
+    echo [INFO] Profiles update...
+    cd /d "!GAME_DIR!"
+    git pull
+
+    echo [INFO] Launching SPT.Server.exe
+    SPT.Server.exe
+
+    echo [INFO] Saving profile changes...
+
+    set "defaultMsg=Auto-commit after server shutdown"
+    set /p "commitMsg=Enter commit message (default: !defaultMsg!): "
+    if "!commitMsg!" == "" (
+        set "commitMsg=!defaultMsg!"
+    )
+
+    git diff --cached --quiet
+    if !errorlevel! == 0 (
+        echo [INFO] No changes, commit skipped.
+    ) else (
+        git commit -m "!commitMsg!"
+        echo [INFO] Pushing changes to the repository...
+        git push origin main
+    )
+)
+
+if "!RESPONDING!" == "1" (
+    echo [INFO] Setting URL in config.json: !SELECTED_URL!
+
+    > "!GAME_DIR!\user\launcher\config_tmp.json" (
+        for /f "usebackq delims=" %%L in ("!GAME_DIR!\user\launcher\config.json") do (
+            echo %%L | findstr /c:"\"Url\"" >nul
+            if !errorlevel! == 0 (
+                echo     "Url": "!SELECTED_URL!",
+            ) else (
+                echo %%L
+            )
+        )
+    )
+
+    move /Y "!GAME_DIR!\user\launcher\config_tmp.json" "!GAME_DIR!\user\launcher\config.json" >nul
+
+    echo [INFO] Launching the game...
+    start "" "!GAME_DIR!\SPT.Launcher.exe"
+    exit /b 0
+)
